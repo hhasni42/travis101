954b5b676d674a8f0ad8868e9605e2ae46a4bd57
Hhasni <hamza.hasni3@fr.bim.com>
2017-12-19 10:40:18 +0100

    python hello world

--- init.sh
+++ init.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -it ubuntu bash
+# docker run -it ubuntu bash
+docker build -t friendlyhello .
+docker images
+docker run -d -p 4000:80 friendlyhello
+docker container ls
+curl http://localhost:4000
