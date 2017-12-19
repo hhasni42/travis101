b60417be1721a3a121f292b4dcc0126ae9e86ded
Hhasni <hamza.hasni3@fr.bim.com>
2017-12-19 15:32:41 +0100

    fix13

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # docker run -it ubuntu bash
-docker build -t friendlyhello . ; docker images ; docker run -d -p 4000:80 friendlyhello ; sleep 30 ; docker container ls ; curl http://google.com
+sleep 30 ; docker container ls ; curl http://google.com
